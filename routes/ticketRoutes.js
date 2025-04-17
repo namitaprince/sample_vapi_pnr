8e81091f8432b9c09e8e9fdfa169dddada69116d
namitaprince <namitaprince2017@gmail.com>
2025-04-17 15:20:49 +0530

    changed route

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { checkTicketStatus } = require("../controller/ticketController");
 
-router.post("/check-ticket", checkTicketStatus);
+router.get("/check-ticket", checkTicketStatus);
 
 
 module.exports = router;
